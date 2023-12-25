c7ff41cd374b9f7b21edee595aa3722b5bb62306
polfc21 <polfarreny@gmail.com>
2023-12-25 17:10:31 +0100

    Added Token model #5

---
+++ src/main/java/com/gymbo/authenticationservice/domain/model/Token.java
@@ -0,0 +1,21 @@
+package com.gymbo.authenticationservice.domain.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Token {
+    private Long id;
+    private String token;
+    private Boolean isRevoked;
+    private Boolean isExpired;
+    private User user;
+    public Token(String token) {
+        this.token = token;
+        this.isExpired = false;
+        this.isRevoked = false;
+    }
+}
