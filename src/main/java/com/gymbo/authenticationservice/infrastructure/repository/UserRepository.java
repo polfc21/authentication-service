37ced366555679c245b791091c0b121a4d851e28
polfc21 <polfarreny@gmail.com>
2023-12-25 17:09:07 +0100

    Added User repository #5

---
+++ src/main/java/com/gymbo/authenticationservice/infrastructure/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gymbo.authenticationservice.infrastructure.repository;
+
+import com.gymbo.authenticationservice.infrastructure.repository.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
