599b3aef197375ed22d7213c8ac0847c35ab60fc
polfc21 <polfarreny@gmail.com>
2023-12-25 17:17:58 +0100

    Added ApiConstant #5

---
+++ src/main/java/com/gymbo/authenticationservice/infrastructure/controller/constant/ApiConstant.java
@@ -0,0 +1,9 @@
+package com.gymbo.authenticationservice.infrastructure.controller.constant;
+
+public class ApiConstant {
+    private ApiConstant() {
+    }
+    public static final String API = "/api";
+    public static final String V1 = "/v1";
+    public static final String LOGIN = "/login";
+}
