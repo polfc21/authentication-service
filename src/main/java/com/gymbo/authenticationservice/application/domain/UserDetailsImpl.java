f3662899193dc8f2f95c3c72be9983a78a257135
polfc21 <polfarreny@gmail.com>
2023-12-25 17:11:47 +0100

    Added UserDetailsImpl #5

---
+++ src/main/java/com/gymbo/authenticationservice/application/domain/UserDetailsImpl.java
@@ -0,0 +1,59 @@
+package com.gymbo.authenticationservice.application.domain;
+
+import com.gymbo.authenticationservice.domain.model.User;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getEmail() {
+        return this.user.getEmail();
+    }
+
+}
